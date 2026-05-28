06bc1abedb54e727aa2b64aa3f7dca98a4e3095d
Elise Davidson <elisedavidson@MacBookPro-49.lan>
2026-05-27 20:45:05 -0400

    Quartz sync: May 27, 2026, 8:45 PM

--- quartz.ts
+++ quartz.ts
@@ -3,3 +3,13 @@ import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/conf
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
+
+ExternalPlugin.Explorer({
+  mapFn: (node) => {
+    if (node.isFolder) {
+      node.displayName = " " + node.displayName
+    } else {
+      node.displayName = "✤ " + node.displayName
+    }
+  },
+})
